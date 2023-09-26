1ead2d53330434c9053b07b9a7f847cbb9033a29
Kittitouch Lelapiyamit <kittitol@scg.com>
2023-09-26 10:51:49 +0700

    second commit

---
+++ log.py
@@ -0,0 +1,29 @@
+import csv
+import os
+
+file_path = "database/devices.csv"
+fieldnames = ['macID', 'deviceType', 'deviceName', 'Location', 'Controller Type']
+
+def WriteCsv(data_list):
+    # Check if the output file already exists and delete it
+    if os.path.exists(file_path):
+        print("deleting csv")
+        os.remove(file_path)
+    print("writing new csv")
+
+
+    # Open the CSV file for writing
+    with open(file_path, 'w', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        # Write the header row
+        writer.writeheader()
+
+        # Write data rows
+        for inner_list in data_list:
+            if len(inner_list) == len(fieldnames):
+                # Create a dictionary by zipping fieldnames and inner_list
+                data_dict = dict(zip(fieldnames, inner_list))
+                writer.writerow(data_dict)
+            else:
+                print(f"Skipping invalid data: {inner_list}")
---
+++ main.py
@@ -0,0 +1,38 @@
+from PyQt5 import QtWidgets, QtGui
+from PyQt5.QtCore import Qt
+from ui import Ui_MainWindow
+from ui_funciton import *
+import sys
+
+def ConnectUiWithEvent(ui):
+    ui.flashButton.clicked.connect(lambda : Flash_Event(ui)) #done
+    
+    ui.addDeviceButton.clicked.connect(lambda : AddDevice_Event(ui))
+    ui.printNowButton.clicked.connect(lambda : Flash_Event(ui))
+    ui.clearListButton.clicked.connect(lambda : Flash_Event(ui))
+
+def ComboBoxInitialize(ui):
+    ProgramFileComboBoxClick_Event(ui)
+
+def SetTableHeader(ui):
+    PopulateTableView(ui.devicesTableView, header, [])
+
+class MyMainWindow(QtWidgets.QMainWindow):
+    def __init__(self, ui):
+        super().__init__()
+        self.ui = ui  # Store the ui reference
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_Return:
+            AddDevice_Event(self.ui)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    ui = Ui_MainWindow()
+    MainWindow = MyMainWindow(ui)
+    ui.setupUi(MainWindow)
+    ConnectUiWithEvent(ui)
+    ComboBoxInitialize(ui)
+    SetTableHeader(ui)
+    MainWindow.show()
+    sys.exit(app.exec_())
