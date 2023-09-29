aba914b9bef3272cc98c91b275ee6fe52c921d56
Kittitouch Lelapiyamit <kittitouch.fame@gmail.com>
2023-09-29 13:18:43 +0700

    add mac status

--- ui_funciton.py
+++ ui_funciton.py
@@ -7,22 +7,29 @@ import log
 mac_id_list = []
 header = ["macID"]
 
+
 def Flash_Event(ui):
     thr = threading.Thread(target=Flashing_ThreadCallback, args=[ui])
     thr.start()
-    ui.flashStatusLabel.setText("Flash Status : <span style=\"color:yellow\">In Progress</span></p>")
+    ui.flashStatusLabel.setText(
+        "Flash Status : <span style=\"color:yellow\">In Progress</span></p>")
+
 
 def Flashing_ThreadCallback(ui):
     jlink.Power_On()
-    result = jlink.JLink_Program_Flash(os.path.join("program_files", ui.programFileComboBox.currentText()))
+    result = jlink.JLink_Program_Flash(os.path.join(
+        "program_files", ui.programFileComboBox.currentText()))
     if result:
-        ui.flashStatusLabel.setText("Flash Status : <span style=\"color:green\">Success</span></p>")
+        ui.flashStatusLabel.setText(
+            "Flash Status : <span style=\"color:green\">Success</span></p>")
     else:
-        ui.flashStatusLabel.setText("Flash Status : <span style=\"color:red\">Fail</span></p>")
+        ui.flashStatusLabel.setText(
+            "Flash Status : <span style=\"color:red\">Fail</span></p>")
+
 
 def ProgramFileComboBoxClick_Event(ui):
     program_files_folder = "program_files"
-        
+
     # Clear the current items in the combobox
     currentText = ui.programFileComboBox.currentText()
     ui.programFileComboBox.clear()
@@ -31,7 +38,7 @@ def ProgramFileComboBoxClick_Event(ui):
     if os.path.exists(program_files_folder) and os.path.isdir(program_files_folder):
         # Get a list of file names in the folder
         file_names = os.listdir(program_files_folder)
-        
+
         # Filter out only the files (not directories) and add them to the combobox
         for file_name in file_names:
             file_path = os.path.join(program_files_folder, file_name)
@@ -39,14 +46,24 @@ def ProgramFileComboBoxClick_Event(ui):
                 ui.programFileComboBox.addItem(file_name)
                 ui.programFileComboBox.setCurrentText(currentText)
 
+
 def AddDevice_Event(ui):
     global mac_id_list
     global header
     jlink.Power_On()
     mac_id = [jlink.MAC_ID_Check()]
     is_mac_id_duplicated = mac_id in mac_id_list
-    if (mac_id == ['']) | (is_mac_id_duplicated):
+    if (mac_id == ['']):
+        ui.addDeviceStatusLabel.setText(
+            "<span style=\"color:red\">Cant read mac address</span></p>)")
         return
+    elif (is_mac_id_duplicated):
+        ui.addDeviceStatusLabel.setText(
+            "<span style=\"color:red\">mac address is duplicated</span></p>)")
+        return
+    else:
+        ui.addDeviceStatusLabel.setText(
+            "<span style=\"color:green\">Okay</span></p>)")
     mac_id_list.append(mac_id)
     log.LogMacID(mac_id[0], "database/devicesLog.csv")
     if len(mac_id_list) == 3:
@@ -54,17 +71,20 @@ def AddDevice_Event(ui):
         mac_id_list = []
     PopulateTableView(ui.devicesTableView, header, mac_id_list)
 
+
 def ClearList_Event(ui):
     global mac_id_list
     mac_id_list = []
     PopulateTableView(ui.devicesTableView, header, mac_id_list)
-    
+
+
 def PrintNow_Event(ui):
     global mac_id_list
     # todo
     log.WriteCsv(['macID'], mac_id_list, "database/devices.csv")
     mac_id_list = []
     PopulateTableView(ui.devicesTableView, header, mac_id_list)
+
 
 def PopulateTableView(tableView, columnHeaders, data):
     # Create a model and set it for the table view
